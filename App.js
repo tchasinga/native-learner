4e53b2c889451ad8bee45c9ddd70375f1303937d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-24 14:28:10 +0200

    new code is added nwo

--- App.js
+++ App.js
@@ -4,7 +4,17 @@ import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   
   const [myName, setMynamer] = useState("Iwaju")
-  const [Items, setItems]
+  const [Items, setItems] =useState([
+    {key: 1, item: 'Items 1'},
+    {key: 2, item: 'Items 2'},
+    {key: 3, item: 'Items 3'},
+    {key: 4, item: 'Items 4'},
+    {key: 5, item: 'Items 5'},
+    {key: 6, item: 'Items 6'},
+    {key: 7, item: 'Items 7'},
+    {key: 8, item: 'Items 8'},
+    {key: 9, item: 'Items 9'},
+  ]);
 
   const InhandlerChanges =() =>{
     setMynamer("Asiwaju")
